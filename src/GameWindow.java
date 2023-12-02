7e0dddbe466c23b5582e2bdf16d6ac8b535d135e
SnugTop <camjdaly@gmail.com>
2023-12-02 13:26:21 -0500

    Strating over. Initial Setup

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -7,66 +7,19 @@ import java.awt.event.ComponentEvent;
 
 public class GameWindow extends JFrame {
 
-    private PlayArea playArea;
-    private TemperatureDisplay tempDisplayLeft, tempDisplayRight;
-    private JButton addButton, resetButton;
+   private PlayArea playArea;
+   private TemperatureCalculator temperatureCalculator;
 
     public GameWindow() {
-        setTitle("Maxwell's Demon Game");
-        setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Maxwell's Demon");
+        setSize(800, 600);
         setLocationRelativeTo(null);
-    
-        // Add the component listener for handling window resize
-        this.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                // Update the play area size and reposition particles if necessary
-                playArea.handleResize();
-            }
-        });
-    
-        // Initialize components
-        initializeComponents();
-    }
-    private void initializeComponents() {
-        tempDisplayLeft = new TemperatureDisplay();
-        tempDisplayRight = new TemperatureDisplay();
-    
-        TemperatureUpdateListener tempListener = (leftTemp, rightTemp) -> {
-            tempDisplayLeft.setText("Left Temp: " + leftTemp);
-            tempDisplayRight.setText("Right Temp: " + rightTemp);
-        };
-    
-        playArea = new PlayArea(tempListener); // Now tempDisplayLeft and tempDisplayRight are already initialized
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        playArea = new PlayArea();
+        temperatureCalculator = new TemperatureCalculator();
         add(playArea, BorderLayout.CENTER);
-    
-        add(tempDisplayLeft, BorderLayout.WEST);
-        add(tempDisplayRight, BorderLayout.EAST);
-    
-        JPanel buttonPanel = new JPanel();
-        addButton = new JButton("Add Particles");
-        resetButton = new JButton("Reset");
-        buttonPanel.add(addButton);
-        buttonPanel.add(resetButton);
-        add(buttonPanel, BorderLayout.SOUTH);
-    }
-    
-   
-
-    public JButton getAddButton() {
-        return addButton;
-    }
-
-    public JButton getResetButton() {
-        return resetButton;
-    }
-
-    public PlayArea getPlayArea() {
-        return playArea;
-    }
-
-    
         
+}
 }
 
