f9085cd8d265769a26e4b895f269ecf781213f94
SnugTop <camjdaly@gmail.com>
2023-12-05 17:48:22 -0500

    Typo

--- src/Maxwell.java
+++ src/Maxwell.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
  * 
  * I could not figure out how to clone this properly, so I just copied it from
  * my personal Repo. Please let me know if there are any issues with running it.
- * It runs for me with now issues passing many test so please let me know if there are 
+ * It runs for me with no issues, passing many test so please let me know if there are 
  * any issues.
  * 
  * The Maxwell class is responsible for initializing and displaying the game
